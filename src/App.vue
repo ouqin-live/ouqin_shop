967ba9cc5683eb72823123b4df56260159c5e778
欧秦 <1350097693@qq.com>
2019-08-14 22:54:22 +0800

    加入底部导航栏

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,35 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
+    <mt-tabbar v-model="selected">
+      <mt-tab-item id="外卖">
+        <img slot="icon" src="./assets/外卖.png" />
+        外卖
+      </mt-tab-item>
+      <mt-tab-item id="订单">
+        <img slot="icon" src="./assets/订单.png" />
+        订单
+      </mt-tab-item>
+      <mt-tab-item id="发现">
+        <img slot="icon" src="./assets/发现.png" />
+        发现
+      </mt-tab-item>
+      <mt-tab-item id="我的">
+        <img slot="icon" src="./assets/我的.png" />
+        我的
+      </mt-tab-item>
+    </mt-tabbar>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'App'
-}
+  name: "App"
+};
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
--- src/main.js
+++ src/main.js
@@ -3,8 +3,17 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+//引入element-ui
+import ElementUI from 'element-ui' 
+import 'element-ui/lib/theme-chalk/index.css'
+//引入Mint-ui
+import MintUI from 'mint-ui'
+import 'mint-ui/lib/style.css'  
 
 Vue.config.productionTip = false
+
+Vue.use(ElementUI)
+Vue.use(MintUI)
 
 /* eslint-disable no-new */
 new Vue({
